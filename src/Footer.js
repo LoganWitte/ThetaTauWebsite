1d0e80e1bf6c9cd2e5b253d2cac224e0ca7d1479
LoganWitte <loganswitte@gmail.com>
2024-11-09 01:35:20 -0500

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -8,32 +8,32 @@ const Footer = () => {
             <div className="p-3">
                 <strong>Theta Tau</strong>
                 <br />
-                <a href="/PUTLINKHERE" target="_blank" rel="noopener noreferrer" className="hover:underline">Rho Gamma Chapter</a>
+                <a href="https:/loganwitte.vercel.app" target="_blank" rel="noopener noreferrer" className="hover:underline">Rho Gamma Chapter</a>
                 <br />
-                <a href="/PUTLINKHERE" target="_blank" rel="noopener noreferrer" className="hover:underline">University of Central Florida</a>
+                <a href="https:/loganwitte.vercel.app" target="_blank" rel="noopener noreferrer" className="hover:underline">University of Central Florida</a>
                 <br />
-                <a href="/PUTLINKHERE" target="_blank" rel="noopener noreferrer" className="hover:underline">2020 Theta Tau Rho Gamma Chapter</a>
+                <a href="https:/loganwitte.vercel.app" target="_blank" rel="noopener noreferrer" className="hover:underline">2020 Theta Tau Rho Gamma Chapter</a>
             </div>
             <div className="p-3">
                 <strong>General</strong>
                 <br />
-                <a href="/PUTLINKHERE" target="_blank" rel="noopener noreferrer" className="hover:underline">National Theta Tau</a>
+                <a href="https:/loganwitte.vercel.app" target="_blank" rel="noopener noreferrer" className="hover:underline">National Theta Tau</a>
                 <br />
-                <a href="/PUTLINKHERE" target="_blank" rel="noopener noreferrer" className="hover:underline">About</a>
+                <a href="https:/loganwitte.vercel.app" target="_blank" rel="noopener noreferrer" className="hover:underline">About</a>
             </div>
             <div className="p-3">
                 <strong>Recruitment</strong>
                 <br />
-                <a href="/PUTLINKHERE" target="_blank" rel="noopener noreferrer" className="hover:underline">FAQ</a>
+                <a href="https:/loganwitte.vercel.app" target="_blank" rel="noopener noreferrer" className="hover:underline">FAQ</a>
                 <br />
-                <a href="/PUTLINKHERE" target="_blank" rel="noopener noreferrer" className="hover:underline">Apply</a>
+                <a href="https:/loganwitte.vercel.app" target="_blank" rel="noopener noreferrer" className="hover:underline">Apply</a>
             </div>
             <div className="p-3 flex flex-col align-center">
                 <strong className="text-center">Socials</strong>
                 <div className="flex">
-                    <a href="/PUTLINKHERE" target="_blank" rel="noopener noreferrer"><img className="px-1" src={instagram} alt="instagram logo"/></a>
-                    <a href="/PUTLINKHERE" target="_blank" rel="noopener noreferrer"><img className="px-1" src={facebook} alt="facebook logo" /></a>
-                    <a href="/PUTLINKHERE" target="_blank" rel="noopener noreferrer"><img className="px-1" src={twitter} alt="x/twitter logo" /></a>
+                    <a href="https:/loganwitte.vercel.app" target="_blank" rel="noopener noreferrer"><img className="px-1" src={instagram} alt="instagram logo"/></a>
+                    <a href="https:/loganwitte.vercel.app" target="_blank" rel="noopener noreferrer"><img className="px-1" src={facebook} alt="facebook logo" /></a>
+                    <a href="https:/loganwitte.vercel.app" target="_blank" rel="noopener noreferrer"><img className="px-1" src={twitter} alt="x/twitter logo" /></a>
                 </div>
                 
             </div>
