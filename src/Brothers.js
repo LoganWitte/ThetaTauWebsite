0bd7ca9782dd6d80d0662e81ebec9315749a64e0
alexeyplav <plavshudin2017@gmail.com>
2024-11-14 12:04:26 -0500

    Added very beginnings of Brothers page

--- src/Brothers.js
+++ src/Brothers.js
@@ -1,7 +1,24 @@
+import gear from './images/gear.png'
+
+
 export default function Brothers() {
     return(
         <div>
-            Brothers
+
+            <div >
+                <header className="headerContainer">
+                    <span class="dot"></span>
+                    <img  src={gear}  alt="gear" class="overlay-image"></img>
+                    <h1>Meet the Brothers</h1>
+                </header>
+            </div>
+
+            <div className = "bigWhiteText">
+                <h2 className = "bigWhiteText">(Rush Class) 20xx </h2>
+            </div>
+
+
         </div>
     )
 }
+
