b1a8dfec8a23704a06f9fc7435ae33df5d34299d
crabmeat11 <crabmeatjj@gmail.com>
2024-12-02 05:59:09 -0500

    Bcrypt part 2

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, request, redirect, url_for, session
 from flask_cors import CORS
-from routes import app_routes, Bcrypt
+from flask_bcrypt import Bcrypt
+from routes import app_routes
+
 app = Flask(__name__)
 CORS(app)
-Bcrypt.init_app(app)
+bcrypt = Bcrypt(app)  # Instantiate Bcrypt and initialize with app
 app.register_blueprint(app_routes)
+
 if __name__ == '__main__':
     app.run(debug=True)
